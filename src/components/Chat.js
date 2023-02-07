a50ee0b370158b7b4d32364a738e383ce6afc8fe
ItsWachira <brianwachira79@gmail.com>
2023-02-07 20:45:11 +0300

    feat: integrate firebase authentication

---
+++ src/components/Chat.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { db, auth } from '../firebase-config'
+import SendMessage from './SendMessage'
+import SignOut from './SignOut'
+import { collection, getDocs,addDoc,updateDoc, doc,deleteDoc } from "firebase/firestore";
+
+function Chat() {
+    const scroll = useRef()
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
+    return (
+        <div>
+            <SignOut />
+            <div className="msgs">
+                {messages.map(({ id, text, photoURL, uid }) => (
+                    <div>
+                        <div key={id} className={`msg ${uid === auth.currentUser.uid ? 'sent' : 'received'}`}>
+                            <img src={photoURL} alt="" />
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <SendMessage scroll={scroll} />
+            <div ref={scroll}></div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/components/SignIn.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import { signInWithPopup} from "firebase/auth";
+import {auth, provider} from '../firebase-config'
+import Button from '@mui/material/Button';
+ 
+
+
+function SignIn() {
+    const [user, setUser] = useState('')
+
+    const signInWithGoogle =  () => {
+        signInWithPopup(auth,provider)
+        };
+    
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
+            <Button onClick={signInWithGoogle} style={{ padding: '30px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }}>Sign In With Google</Button>
+        </div>
+    )
+}
+
+export default SignIn
